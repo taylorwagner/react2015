c9f1fa4291e8ccbc937bafe98d39e094e1ca8e91
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-11-12 20:57:10 -0600

    eliminate variable

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ console.log(`I'd like one ${fruit}, please`);
 console.log(`Here you go: ${fruit}`);
 console.log(`Delicious! May I have another?`);
 
-let rest = remove(fruit, fruits);
+remove(fruit, fruits);
 
-console.log(`I'm sorry, we're all out. We have ${rest.length} left.`);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
